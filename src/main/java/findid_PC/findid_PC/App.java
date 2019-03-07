1353a2707c7610a57e95399693cb68346be61c8b
Administrator <Administrator@2015080004-3WVP>
2019-03-07 12:27:15 +0900

    최초

---
+++ src/main/java/findid_PC/findid_PC/App.java
@@ -0,0 +1,13 @@
+package findid_PC.findid_PC;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/findid_PC/findid_PC/AppTest.java
@@ -0,0 +1,45 @@
+package findid_PC.findid_PC;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import static com.codeborne.selenide.Selenide.*;
+
+import java.awt.AWTException;
+
+import com.codeborne.selenide.WebDriverRunner;
+
+public class AppTest extends pushbullet_login {
+	public static WebDriver driver;
+
+	@BeforeTest
+	public void setup() {
+		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+		driver = new ChromeDriver();
+		WebDriverRunner.setWebDriver(driver);
+	}
+
+	@Test
+	public void findID() throws AWTException {
+		open("https://front.wemakeprice.com/user/find/id");
+
+		$(By.xpath("//span[contains(.,'확인')]")).click();
+
+		$(By.id("_name")).sendKeys("테스트");
+		$(By.id("_phone")).sendKeys("01043642021");
+
+		$(By.xpath("//span[contains(.,'인증번호 발송')]")).click();
+		Pb();
+		$(By.xpath("//span[contains(.,'확인')]")).click();
+
+	}
+
+	@AfterMethod
+	public void end() {
+		screenshot("account");
+	}
+}
